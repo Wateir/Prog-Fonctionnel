9b492780d2c2c2d3d165ab9e5995bcd50397a57f
Wateir <78731687+Wateir@users.noreply.github.com>
2024-04-04 15:15:10 +0200

    Create Liste-recursivite.mli
    ajout d'un programme de gestion de liste different

---
+++ Liste-recursivite.mli
@@ -0,0 +1,36 @@
+let rec somme l = match l with
+  | a::l' -> if l' = [] then a else a + (somme l')
+  | _ -> failwith "bin non";;
+
+BEST VERSION 
+
+let rec somme l = match l with
+  | [] -> 0
+  | a::l' -> a + somme l';;
+
+
+let rec longueur l = match l with
+  | [] -> 0
+  | a::l' -> 1 + longueur l';;
+
+let dernier l = let rec aux l = match l with
+    | a::[]-> a
+    | a::l' -> aux l'
+  in if l = [] then failwith "dernier : liste vide"
+  else
+    aux l;;
+
+let rec append l1 l2 = match l1 with 
+  | [] -> l2
+  |l3::l4 -> l3::(append l4 l2);;
+
+
+let rec reverse l = match l with
+  |[] ->[] 
+  |a::l' -> reverse l'@[a];;
+
+let reversebis l = let rec aux bis l = match l with
+    | [] -> res
+    | x::l' -> aux x::res l'
+  in aux [] l;;
+ 
