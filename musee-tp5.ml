2504525dee871e6c563f4a5743ad1c0342d44e5c
Wateir <78731687+Wateir@users.noreply.github.com>
2024-04-14 01:27:21 +0200

    Update musee-tp5.ml
    Correction des differents bugs

--- musee-tp5.ml
+++ musee-tp5.ml
@@ -10,25 +10,23 @@ let musee = [
 ]
 
 let rec insertObj element listecouple = match listecouple with
-  |[] -> (1, element)::listecouple
-  |(a,el)::listebis -> if el  == element 
-      then
+  | [] -> [(1, element)]
+  | (a, el)::listebis -> if el = element then
         (a+1, element)::listebis
       else
-        insertObj element listecouple;;
+        (a, el)::(insertObj element listebis);;
 
-let rec  insert index element listeindex = match listeindex with
-  | [] -> (index, [1, element])::listeindex
-  |(idx, listecouple)::resteliste -> if idx == index
-      then
-        (index, (insertObj element listecouple))::resteliste
+let rec insert index element listeindex = match listeindex with
+  | [] -> [(index, [(1, element)])]
+  | (idx, listecouple)::resteliste -> if idx = index then
+        (idx, insertObj element listecouple)::resteliste
       else
-        insert index element resteliste;;
+        (idx, listecouple)::(insert index element resteliste);;
 
-let rec decompte listeidxobjc = match listeidxobjc with
+let rec decompte liste = match liste with
   | [] -> []
-  | (a, b)::resteliste -> [insert a, b (decompte resteliste)];;
-
+  | (idx, obj)::resteliste -> insert idx obj (decompte resteliste);;
+                                
 let rec longueur liste = match liste with
   |[] -> 0
   |(a, b)::resteliste -> a + longueur resteliste ;;
@@ -41,5 +39,4 @@ let rec aumoins_indexed n liste = match liste with
       else
         aumoins_indexed n resteliste;;
 
-let aumoins n liste = aumoins_indexed n (decompte liste)
-
+let aumoins n liste = aumoins_indexed n (decompte liste);;
